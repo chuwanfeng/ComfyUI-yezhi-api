41b9ac30c17c359ff09c52346f4ec47d5d1330b7
yezhi <chuwanfeng@hotmail.com>
2026-07-05 00:48:24 +0800

    chore: 清理临时脚本

--- _fixfont.js
+++
@@ -1,6 +0,0 @@
-const fs = require('fs');
-let css = fs.readFileSync('D:\\vps\\python\\ComfyUI-yezhi-api\\static\\css\\app.css', 'utf8');
-css = css.replace(/"MaShanZheng"/g, '"Ma Shan Zheng"');
-css = css.replace(/"NotoSerifSC"/g, '"Noto Serif SC"');
-fs.writeFileSync('D:\\vps\\python\\ComfyUI-yezhi-api\\static\\css\\app.css', css, 'utf8');
-console.log('Done. Size:', css.length);
